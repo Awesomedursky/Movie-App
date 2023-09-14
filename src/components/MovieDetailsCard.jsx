5cab7d12e8c5b05200355bb11ec2d9e096023710
Ayodele Ayodeji <awesomedursky@gmail.com>
2023-09-14 16:19:52 +0100

    another commit

--- src/components/MovieDetailsCard.jsx
+++ src/components/MovieDetailsCard.jsx
@@ -6,15 +6,15 @@ const MovieDetailsCard = ({ poster, title, time, genre, runtime, preview }) => {
 
   return (
     <div className='flex flex-col md:items-start flex-1 gap-y-5'>
-      <img src={`https://image.tmdb.org/t/p/original${poster}`} alt={`${poster}`} className='h-[60vh] md:h-[25rem] md:w-[20rem] rounded-2xl' />
-      <div className='max-w-[40rem] flex flex-col items-start'>
+      <img src={`https://image.tmdb.org/t/p/original${poster}`} alt={`${poster}`} className='h-[20vh] md:h-[20rem] md:w-[20rem] rounded-2xl' />
+      <div className='max-w-[50rem] flex flex-col items-start'>
         <h3 data-testid='movie-title' className=' text-[#666] text-2xl font-bold'>
           {title}
         </h3>
         <p data-testid='movie-release-date' className=' text-[#666] text-lg font-bold'>
           {timeUTC(`${time}`)}
         </p>
-        <p data-testid='movie-genre' className="flex gap-x-2">
+        <p data-testid='movie-genre' className='flex gap-x-2'>
           {genre}
         </p>
 
